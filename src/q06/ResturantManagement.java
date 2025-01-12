5790b6416e7ec5190839e8119f5b07fc83ef23fe
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-12 16:06:56 +0530

    Update ResturantManagement.java

--- src/q06/ResturantManagement.java
+++ src/q06/ResturantManagement.java
@@ -1,11 +1,15 @@
 public class ResturantManagement {
+    
     public static void main(String[] args) {
-    Dish dish1 = new Dish("Pasta", 550.00);
-    Dish dish2 = new Dish("Pizza", 1200.00);
-    Restaurant restaurant = new Restaurant(3);
-    restaurant.addOrder(new Order(dish1, 2));
-    restaurant.addOrder(new Order(dish2, 1));
-    restaurant.addOrder(new Order(dish2, 5));
-    restaurant.addOrder(new Order(dish1, 3));
+        
+        Dish dish1 = new Dish("Pasta", 550.00);
+        Dish dish2 = new Dish("Pizza", 1200.00);
+        
+        Restaurant restaurant = new Restaurant(3);
+    
+        restaurant.addOrder(new Order(dish1, 2));
+        restaurant.addOrder(new Order(dish2, 1));
+        restaurant.addOrder(new Order(dish2, 5));
+        restaurant.addOrder(new Order(dish1, 3));
     }
 }
