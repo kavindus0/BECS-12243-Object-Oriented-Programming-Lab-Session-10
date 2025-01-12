b37e1a3323cb632cc86a3b2cbc64ec9842043c82
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-12 16:02:35 +0530

    Update PointClient.java

--- src/q02/PointClient.java
+++ src/q02/PointClient.java
@@ -1,4 +1,5 @@
 package q02;
+
 public class PointClient { 
 
     public static void main(String[] args) { 
@@ -6,9 +7,7 @@ public class PointClient {
         Point p1 = new Point(20, 2); 
 
         Point p2 = new Point(3, 6); 
-
          
-
         System.out.println("Point 1: " + p1.displayPoint()); 
 
         System.out.println("Point 2: " + p2.displayPoint()); 
